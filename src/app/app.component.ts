c9cf01c33430aa6c22276e7b2f2c9c05254aa918
Charl913 <charlkruger61@gmail.com>
2023-10-14 09:47:47 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'assignment_3';
+export class AppComponent{
+  incrementClicks : number[] = [];
+
+  isDisplayed = true;
+
+  num = 0;
+
+
+  toggleDisplay() {
+    this.isDisplayed === true ? this.isDisplayed = false : this.isDisplayed= true
+
+    this.num ++;
+
+    this.incrementClicks.push(this.num);
+  }
 }
